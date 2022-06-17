fd462ffdfc475344201dc5867c8ad0ea38fd8b6e
deepu1067 <dshahadat10@gmail.com>
2022-06-17 17:07:49 +0600

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 var image = document.getElementById('img')
+var article = document.getElementsByClassName('article')[0];
+// console.log(article.id);
 
-image.onload = function(){
-    var article = document.getElementsByClassName('article')[0];
-    article.className = 'article slide_top';
-}
+// image.onload = function(){
+
+//     if(article.id == 'top'){
+//         article.className = 'article slide_top';
+//     }
+//     else if(article.id == 'left'){
+//         article.className = 'article slide_left';
+//     }
+// }
+
+window.addEventListener('load', function(){
+    if(article.id == 'top'){
+        article.className = 'article slide_top';
+    }
+    else if(article.id == 'left'){
+        article.className = 'article slide_left';
+    }
+})
